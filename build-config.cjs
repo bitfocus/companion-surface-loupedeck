28d3d9ed6c12ac10dc3bbf4bf142c0c6cdcedbfb
Julian Waller <git@julusian.co.uk>
2025-11-01 18:07:22 +0000

    fix: serialport loading

--- build-config.cjs
+++ build-config.cjs
@@ -1,4 +1,8 @@
 module.exports = {
 	forceRemoveNodeGypFromPkg: true,
-	prebuilds: ['@serialport/bindings-cpp'],
+	// TODO: Ideally this would use prebuilds instead of externals, but that requires some mangling of the loading as it is looking in ../ for the prebuilds dir
+	externals: {
+		'@serialport/bindings-cpp': 'commonjs2 @serialport/bindings-cpp',
+	},
+	// prebuilds: ['@serialport/bindings-cpp'],
 }
