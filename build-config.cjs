5d2ec6c8b85ae27f77e8ab42b00898dc60102f75
Julian Waller <git@julusian.co.uk>
2026-04-25 16:35:13 +0100

    feat: update tooling and target node 26

--- build-config.cjs
+++ build-config.cjs
@@ -1,8 +1,6 @@
 module.exports = {
 	forceRemoveNodeGypFromPkg: true,
 	// TODO: Ideally this would use prebuilds instead of externals, but that requires some mangling of the loading as it is looking in ../ for the prebuilds dir
-	externals: {
-		'@serialport/bindings-cpp': 'commonjs2 @serialport/bindings-cpp',
-	},
+	externals: ['@serialport/bindings-cpp'],
 	// prebuilds: ['@serialport/bindings-cpp'],
 }
